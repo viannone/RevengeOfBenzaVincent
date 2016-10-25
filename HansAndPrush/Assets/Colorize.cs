01e9f7ff7ed33111ff38792f1cde1f9c5b19c47b
viannone <VIannone@Vincents-iMac.local>
2016-10-25 00:58:02 -0700

    Added Colorization

---
+++ HansAndPrush/Assets/Colorize.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Colorize : MonoBehaviour {
+
+	public Color targetColor = Color.red;
+	public float changeRate = 0.1f;
+	public bool isColored = false;
+	public string color;
+	SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+		GameObject.FindGameObjectWithTag (color).GetComponent<ColorSwitch>().AddToList(this);
+	}
+
+	public void ColorMe(){
+		StartCoroutine ("Colorify");
+	}
+
+
+	public IEnumerator Colorify(){
+		Color currColor = sr.color;
+		Color oldColor = currColor;
+		float t = 0.0f;
+		while(currColor.r > targetColor.r){
+			t += Time.deltaTime;
+			currColor.r = Mathf.Lerp(oldColor.r, targetColor.r, changeRate * t);
+			sr.color = currColor;
+			yield return null;
+		}
+		t = 0.0f;
+		oldColor = sr.color;
+		while (currColor.g > targetColor.g) {
+			t += Time.deltaTime;
+			currColor.g = Mathf.Lerp (oldColor.g, targetColor.g, changeRate * t);
+			sr.color = currColor;
+			yield return null;
+		}
+		t = 0.0f;
+		oldColor = sr.color;
+		while (currColor.b > targetColor.b) {
+			t += Time.deltaTime;
+			currColor.b = Mathf.Lerp (oldColor.b, targetColor.b, changeRate * t);
+			sr.color = currColor;
+			yield return null;
+		}
+		isColored = true;
+	}
+}
---
+++ HansAndPrush/Assets/ShadowFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowFollow : MonoBehaviour {
+	public Transform target;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.transform.position;
+	}
+}
